3d6e657c429e52531ede6d64cfceeb2f3cd74a25
Daukaevt <mupbam1@gmail.com>
2021-07-30 23:22:08 +0300

    quote string

---
+++ app/src/main/java/com/wixsite/mupbam1/resume/sqlite_kotlin/db/MyDbNameClass.kt
@@ -0,0 +1,10 @@
+package com.wixsite.mupbam1.resume.sqlite_kotlin.db
+
+import android.provider.BaseColumns
+
+object MyDbNameClass: BaseColumns {
+    const val TABLE_NAME = "stocks"
+    const val COLUMN_NAME_DATA = "date"
+    const val COLUMN_NAME_TICKER1 = "APPL"
+
+}
